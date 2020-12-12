6a72f213d6a87871cec26da0890d36cb055cb314
igorcruzz <igorskt2009@gmail.com>
2020-12-12 00:59:36 -0300

    feat: create signup Route

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -14,10 +14,6 @@ import {
   IUserState,
 } from '../../store/ducks/repositories/user/types'
 
-interface Errors {
-  [key: string]: string
-}
-
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
   const loading = useSelector((state: IUserState) => state.user.loading)
@@ -26,19 +22,9 @@ const SignUp: React.FC = () => {
   const handleSubmit = async (data: IUserCreate) => {
     try {
       const schema = Yup.object().shape({
-        name: Yup.string()
-          .min(5, 'O nome precisa ter no mínimo 5 caracteres')
-          .required('Campo obrigatório'),
         email: Yup.string()
           .email('Insira um e-mail válido')
           .required('Campo obrigatório'),
-        password: Yup.string()
-          .min(6, 'O campo senha precisa ter no mínimo 6 caracteres')
-          .required('Campo obrigatório'),
-        confirmPassword: Yup.string()
-          .oneOf([Yup.ref('password')], 'O campo de senha não bate')
-          .required('Campo obrigatório')
-          .typeError('O campo de senha não bate'),
       })
 
       await schema.validate(data, { abortEarly: false })
@@ -62,15 +48,7 @@ const SignUp: React.FC = () => {
         </Title>
 
         <Form onSubmit={handleSubmit} ref={formRef}>
-          <Input name="name" placeholder="Nome" />
           <Input name="email" type="email" placeholder="E-mail" />
-          <Input name="password" type="password" placeholder="Senha" />
-          <Input
-            name="confirmPassword"
-            type="password"
-            placeholder="Confirmar Senha"
-          />
-
           <button type="submit">
             {loading ? 'Carregando...' : 'Cadastrar'}
           </button>
