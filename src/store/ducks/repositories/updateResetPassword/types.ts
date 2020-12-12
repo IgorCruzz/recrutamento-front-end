3e0c55a7d4b1b5f7cca3e49d732ce09138aa7eb6
igorcruzz <igorskt2009@gmail.com>
2020-12-12 15:55:33 -0300

    feat: create updateresetpassword types

---
+++ src/store/ducks/repositories/updateResetPassword/types.ts
@@ -0,0 +1,18 @@
+export enum RepositoriesTypes {
+  UPDATE_RESET_PASSWORD_REQUEST = '@updateResetPassword/UPDATE_RESET_PASSWORD_REQUEST',
+  UPDATE_RESET_PASSWORD_SUCCESS = '@updateResetPassword/UPDATE_RESET_PASSWORD_SUCCESS',
+  UPDATE_RESET_PASSWORD_FAILURE = '@updateResetPassword/UPDATE_RESET_PASSWORD_FAILURE',
+}
+
+export interface IUpdateResetPassword {
+  token: string
+  email: string
+  password: 'string'
+  confirmPassword: 'string'
+}
+
+export interface IUpdateesetPasswordState {
+  updateResetPassword: {
+    loading: boolean
+  }
+}
