bd93f667bafcc3ec870b3456745ec6b689f30099
igorcruzz <igorskt2009@gmail.com>
2020-12-12 16:56:35 -0300

    test: create createPassword tests

---
+++ src/__tests__/store/reducer/createPassword.test.ts
@@ -0,0 +1,45 @@
+import {
+  INITIAL_VALUES,
+  createPassword,
+} from '../../../store/ducks/repositories/createPassword/reducer'
+import {
+  createPasswordFailure,
+  createPasswordRequest,
+  createPasswordSuccess,
+} from '../../../store/ducks/repositories/createPassword/actions'
+
+describe('createPassword', () => {
+  it('createPasswordRequest', () => {
+    const updateResetPasswordData = {
+      token: 'token',
+      email: 'user@email.com',
+      password: 'password',
+      confirmPassword: 'password',
+    }
+
+    const state = createPassword(
+      INITIAL_VALUES,
+      createPasswordRequest(updateResetPasswordData)
+    )
+
+    expect(state).toStrictEqual({ loading: true })
+  })
+
+  it('createPasswordSuccess', () => {
+    const state = createPassword(INITIAL_VALUES, createPasswordSuccess())
+
+    expect(state).toStrictEqual({ loading: false })
+  })
+
+  it('createPasswordFailure', () => {
+    const state = createPassword(INITIAL_VALUES, createPasswordFailure())
+
+    expect(state).toStrictEqual({ loading: false })
+  })
+
+  it('DEFAULT', () => {
+    const state = createPassword(undefined, {})
+
+    expect(state).toStrictEqual(INITIAL_VALUES)
+  })
+})
