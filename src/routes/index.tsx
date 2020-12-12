4581569d6c455016e42154965f7cfc228c8819aa
igorcruzz <igorskt2009@gmail.com>
2020-12-12 01:08:21 -0300

    feat: create routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,18 +2,14 @@ import React from 'react'
 import { Switch } from 'react-router-dom'
 import Route from './routes'
 
-import Dashboard from '../pages/dashboard'
 import Signin from '../pages/signin'
 import SignUp from '../pages/signup'
-import Authentication from '../pages/authentication'
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Signin} />
       <Route path="/signup" component={SignUp} />
-      <Route path="/dashboard" component={Dashboard} isPrivate />
-      <Route path="/auth/:token" component={Authentication} />
     </Switch>
   )
 }
