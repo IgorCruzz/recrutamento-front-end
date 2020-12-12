6f19fe7203f2cb26d16176139fdcc9f4cdd00a84
igorcruzz <igorskt2009@gmail.com>
2020-12-12 01:31:40 -0300

    chore: add reactotron

--- src/pages/_layouts/default/index.tsx
+++ src/pages/_layouts/default/index.tsx
@@ -1,13 +1,7 @@
 import React, { ReactNode } from 'react'
 
 import { Wrapper } from './styles'
-import { Header } from '../../../components/header'
 
 export default function DefaultLayout({ children }: { children: ReactNode }) {
-  return (
-    <Wrapper>
-      <Header />
-      {children}
-    </Wrapper>
-  )
+  return <Wrapper>{children}</Wrapper>
 }
